c001d80605ae6ba7170d9001989d8c1b7db59365
Carlos Bernal Torres <carlos.bernaltorres@ucalgary.ca>
2024-12-09 04:10:33 -0700

    feat: added user info to account page

--- src/routes/home/account/+page.svelte
+++ src/routes/home/account/+page.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
-    import { json, text } from "@sveltejs/kit";
-    import { preventDefault } from "svelte/legacy";
- </script>
- <h1>Personal Information </h1>
+  import { json, text } from '@sveltejs/kit'
+  import { preventDefault } from 'svelte/legacy'
+  import { getUserContext } from '../../../context'
+  ;("I know you said to let you try, but idc, here's the info of the currently logged in user 😊")
+  let user = getUserContext()
+</script>
+
+<h1>Personal Information</h1>
 
 <form>
-<div id = div1>
-<br>
-    <label for = "fname">First name :</label> 
-    <input id = "fname" type = "text" disabled>
-    <script> fetch (`http://localhost:5000/add-employee'`)
+  <div id="div1">
+    <br />
+    <label for="fname">First name :</label>
+    <input id="fname" type="text" disabled />
+    <script>
+ fetch (`http://localhost:5000/add-employee'`)
         .then (Response => response.json())
         .then (data => {
             document.getElementbyID ("fname").value = data.name;
@@ -19,11 +24,12 @@
       return 
     }
     </script>
-<br>
-<br>
-    <label for = "lname">Last Name: </label>
-    <input id = "lname" type = "text" disabled>
-    <script> fetch (`http://localhost:5000/add-employee'`)
+    <br />
+    <br />
+    <label for="lname">Last Name: </label>
+    <input id="lname" type="text" disabled />
+    <script>
+ fetch (`http://localhost:5000/add-employee'`)
         .then (Response => response.json())
         .then (data => {
             document.getElementbyID ("lname").value = data.message;
@@ -33,51 +39,50 @@
       return 
     }
     </script>
-<br>
+    <br />
 
-<br>
-    <label for = "SIN"> SIN: </label>
-    <input id = "SIN" type = "text" disabled>
-    
-<br>
+    <br />
+    <label for="SIN"> SIN: </label>
+    <input id="SIN" type="text" disabled />
 
-<br>
-    <label for = "email">Email: </label>
-    <input id = "email" type ="email" disabled>
-<br>
+    <br />
 
-<br>
-    <label for = "phone"> Phone number: </label>
-    <input id = "phone" type = "tel" >
-<br>
+    <br />
+    <label for="email">Email: </label>
+    <input id="email" type="email" disabled />
+    <br />
 
-<br>
-    <label for = "address"> Address: </label>
-    <input id = "address" type = "text" >
-<br>
+    <br />
+    <label for="phone"> Phone number: </label>
+    <input id="phone" type="tel" />
+    <br />
 
-<br>
-    <label for = "password"> Password: </label>
-    <input id = "password" type = "password" >
-<br>
+    <br />
+    <label for="address"> Address: </label>
+    <input id="address" type="text" />
+    <br />
 
-<br>
-    <label for = "departmentID"> Department ID: </label>
-    <input id = "departmentID" type = "Text" disabled >
-<br>
+    <br />
+    <label for="password"> Password: </label>
+    <input id="password" type="password" />
+    <br />
 
-<br>
-    <label for = "rate"> Rate: </label>
-    <input id = "rate" type = "text" disabled >
-<br>
-</div>
+    <br />
+    <label for="departmentID"> Department ID: </label>
+    <input id="departmentID" type="Text" disabled />
+    <br />
 
-<div id = div2>
-<br>
-<input id = "submit" type = "button" name = "Submit" value = "Submit">
-<br>
-</div>
+    <br />
+    <label for="rate"> Rate: </label>
+    <input id="rate" type="text" disabled />
+    <br />
+  </div>
 
+  <div id="div2">
+    <br />
+    <input id="submit" type="button" name="Submit" value="Submit" />
+    <br />
+  </div>
 </form>
 
 <!-- <script>
@@ -95,15 +100,17 @@
 <!-- HTML -->
 
 <style>
-h1{text-align:center}
-#div1 {
-    position:bottom;
+  h1 {
+    text-align: center;
+  }
+  #div1 {
+    position: bottom;
     left: 30 px;
     width: 600px;
     padding: 0px;
-    text-align:right;
-}
-#div2{
-    text-align:center;
-}
+    text-align: right;
+  }
+  #div2 {
+    text-align: center;
+  }
 </style>
